8333d70f8d9f18f30f5e099310c2ed4e054b5199
Richard <62489312+Richard172@users.noreply.github.com>
2020-08-26 16:44:35 -0500

    change by Richard

--- battleship.cpp
+++ battleship.cpp
@@ -1,16 +1,15 @@
 // a battleship game made by Richard, Tej, and Arjun
-#include "normal_AI.hpp"
+// #include "normal_AI.hpp"
+// #include "difficult_AI.hpp"
+#include "randomize_algorithm.hpp"
+
 
 int main()
 {
 	int player[8][8];  // a 8 by 8 matrix for the game board for player
 	int ai[8][8];  // the grid for AI
 
-	zero_grid(player);
 	
-	int* test = normal_ai(player);
-	std::cout << test[0] << ' ' << test[1] << std::endl;
-	print_grid(player);
 	
 	return 0;
 }
